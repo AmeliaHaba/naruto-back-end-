900e5804ed84a7a18fb9f07b9672f9bcc2e40daa
Amelia Haba <amelia.haba347@gmail.com>
2021-07-28 08:59:04 -0400

    update Kakashi name in seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,102 +8,89 @@
 
 Character.destroy_all
 
-
 Character.create!(
-    name: "Naruto Uzumaki"
-    bio: "Naruto is a young boy with blonde hair. Naruto has dreams of becoming Hokage one day. He is a powerful and confident ninja who posses the nine tailed fox within him."
-    village: "Hidden Leaf Village"
-    team: "Team 7 (Kakashi)"
-    img:
+    name: "Naruto Uzumaki",
+    bio: "Naruto is a young boy with blonde hair. Naruto has dreams of becoming Hokage one day. He is a powerful and confident ninja who posses the nine tailed fox within him.",
+    village: "Hidden Leaf Village",
+    team: "Kakashi"
 )
 
 Character.create!(
-    name: "Sasuke Uchiha"
-    bio: "Sasuke is a very talented ninja with black hair and a determined look. He posses the Sharingan, which allows him to see his opponent's technique. "
-    village: "Hidden Leaf Village"
-    team: "Team 7 (Kakashi)"
-    img:
+    name: "Sasuke Uchiha",
+    bio: "Sasuke is a very talented ninja with black hair and a determined look. He posses the Sharingan, which allows him to see his opponent's technique.",
+    village: "Hidden Leaf Village",
+    team: "Kakashi"
 )
 
 Character.create!(
-    name: "Sakura Haruno"
-    bio: "Sakura is a young and smart ninja who has light pink hair. She is known for her good grades, her love for Sasuke, and at times can be self-conscious. "
-    village: "Hidden Leaf Village"
-    team: "Team 7 (Kakashi)"
-    img:
+    name: "Sakura Haruno",
+    bio: "Sakura is a young and smart ninja who has light pink hair. She is known for her good grades, her love for Sasuke, and at times can be self-conscious.",
+    village: "Hidden Leaf Village",
+    team: "Kakashi"
 )
 
 Character.create!(
-    name: "Ino Yamanaka"
-    bio: "Ino is a young Ninja with long blonde hair and a purple outfit. She has exceptional chakra control and can sense other's changes in their chakra. "
-    village: "Hidden Leaf Village"
-    team: "Team 10 (Asuma)"
-    img:
+    name: "Ino Yamanaka",
+    bio: "Ino is a young Ninja with long blonde hair and a purple outfit. She has exceptional chakra control and can sense other's changes in their chakra.",
+    village: "Hidden Leaf Village",
+    team: "Asuma"
 )
 
 Character.create!(
-    name: "Shikamaru Nara"
-    bio: "Shikamaru is known for his laziness and high IQ. He is an extremely strategic and smart ninja who calculates each of his moves. Though he isn't very motivated, he is a reliable teammate who will help out his team when needed."
-    village: "Hidden Leaf Village"
-    team: "Team 10 (Asuma)"
-    img:
+    name: "Shikamaru Nara",
+    bio: "Shikamaru is known for his laziness and high IQ. He is an extremely strategic and smart ninja who calculates each of his moves. Though he isn't very motivated, he is a reliable teammate who will help out his team when needed.",
+    village: "Hidden Leaf Village",
+    team: "Asuma"
 )
 
 Character.create!(
-    name: "Choji Akimichi"
-    bio: "Choji is known for his love of food and eating. He is highly food motivated in missions. He is bestfriends with Shikamaru and can convert calories into chakra."
-    village: "Hidden Leaf Village"
-    team: "Team 10 (Asuma)"
-    img:
+    name: "Choji Akimichi",
+    bio: "Choji is known for his love of food and eating. He is highly food motivated in missions. He is bestfriends with Shikamaru and can convert calories into chakra.",
+    village: "Hidden Leaf Village",
+    team: "Asuma"
 )
 
 Character.create!(
-    name: "Tenten"
-    bio: "Tenten is a strong ninja who is known for her use of weapons and tools while fighting. "
-    village: "Hidden Leaf Village"
-    team: "Team Guy"
-    img:
+    name: "Tenten",
+    bio: "Tenten is a strong ninja who is known for her use of weapons and tools while fighting.",
+    village: "Hidden Leaf Village",
+    team: "Guy"
 )
 
 Character.create!(
-    name: "Rock Lee"
-    bio: "Though Rock doesn't have talent for ninjutsu and genjutsu, he is a skilled master in taijutsu. He idolizes his Guy sensei and even emulates his looks. He is optimistic, energetic, and always keeps his promise."
-    village: "Hidden Leaf Village"
-    team: "Team Guy"
-    img:
+    name: "Rock Lee",
+    bio: "Though Rock doesn't have talent for ninjutsu and genjutsu, he is a skilled master in taijutsu. He idolizes his Guy sensei and even emulates his looks. He is optimistic, energetic, and always keeps his promise.",
+    village: "Hidden Leaf Village",
+    team: "Guy"
 )
 
 
 Character.create!(
-    name: "Neji Hyūga"
-    bio: "Neji is a prodigy of the Hyūga clan, even though he is a member of the clan's branch family and his duty is to always serve the main family of the house. His Byakugan is especially powerful. "
-    village: "Hidden Leaf Village"
-    team: "Team Guy"
-    img:
+    name: "Neji Hyūga",
+    bio: "Neji is a prodigy of the Hyūga clan, even though he is a member of the clan's branch family and his duty is to always serve the main family of the house. His Byakugan is especially powerful.",
+    village: "Hidden Leaf Village",
+    team: "Guy"
 )
 
 Character.create!(
-    name: "Hinata Hyūga"
-    bio: "Hinata is a shy and gentle ninja who is in love with Naruto. She is very kind and always thinking of others. She also possess the Byakugan and proves to become a very strong ninja with excellent chakra control."
-    village: "Hidden Leaf Village"
-    team: "Team 8 (Kurenai)"
-    img:
+    name: "Hinata Hyūga",
+    bio: "Hinata is a shy and gentle ninja who is in love with Naruto. She is very kind and always thinking of others. She also possess the Byakugan and proves to become a very strong ninja with excellent chakra control.",
+    village: "Hidden Leaf Village",
+    team: "Kurenai"
 )
 
 Character.create!(
-    name: "Kiba Inuzuka"
-    bio: "Kiba can be short-tempered and impulsive but is a very strong ninja. He always has his dog, Akamaru, with him. Kiba is known for fighting alongside his dog and using the Four Legs technique."
-    village: "Hidden Leaf Village"
-    team: "Team 8 (Kurenai)"
-    img:
+    name: "Kiba Inuzuka",
+    bio: "Kiba can be short-tempered and impulsive but is a very strong ninja. He always has his dog, Akamaru, with him. Kiba is known for fighting alongside his dog and using the Four Legs technique.",
+    village: "Hidden Leaf Village",
+    team: "Kurenai"
 )
 
 Character.create!(
-    name: "Shino Aburame"
-    bio: "Shino is a calm, quiet, and mysterious person. He is very stoic and matter-of-fact. At birth he was infested with insects that feed on his chakra. During battle, he uses these insects to defeat opponents."
-    village: "Hidden Leaf Village"
-    team: "Team 8 (Kurenai)"
-    img:
+    name: "Shino Aburame",
+    bio: "Shino is a calm, quiet, and mysterious person. He is very stoic and matter-of-fact. At birth he was infested with insects that feed on his chakra. During battle, he uses these insects to defeat opponents.",
+    village: "Hidden Leaf Village",
+    team: "Kurenai"
 )
 
 
